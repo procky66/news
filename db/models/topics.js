f7b6314c135143afe2292c46162ca1f23e171293
procky66 <steveprocter69@googlemail.com>
2022-09-05 15:24:02 +0100

    Removed check for no items found from users and topics to allow this to be dealt with in front end

--- db/models/topics.js
+++ db/models/topics.js
@@ -1,13 +1,7 @@
 const db = require("../connection");
 
 exports.fetchTopics = () => {
-
 	const queryStr = `SELECT * FROM topics;`;
 
-	return db.query(queryStr).
-	then(results => {
-		if (results.rows.length === 0) {
-			return Promise.reject({ status: 404, msg: "no topics found" });
-		} else return results.rows;
-	});;
+	return db.query(queryStr).then(results => results.rows);
 };
--- db/models/users.js
+++ db/models/users.js
@@ -1,13 +1,7 @@
 const db = require("../connection");
 
 exports.fetchUsers = () => {
-
 	const queryStr = `SELECT * FROM users;`;
 
-	return db.query(queryStr).
-	then(results => {
-		if (results.rows.length === 0) {
-			return Promise.reject({ status: 404, msg: "no users found" });
-		} else return results.rows;
-	});;
+	return db.query(queryStr).then(results => results.rows);
 };
