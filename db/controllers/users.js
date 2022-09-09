a92d2420f858dc7071c458f9767ced759b61a58b
procky66 <steveprocter69@googlemail.com>
2022-09-09 16:18:50 +0100

    17. GET /api/users/:username

--- db/controllers/users.js
+++ db/controllers/users.js
@@ -1,10 +1,20 @@
-const { fetchUsers } = require("../models/users");
+const { fetchUsers, fetchUserById } = require("../models/users");
 
 exports.getUsers = (req, res, next) => {
 
 	fetchUsers()
 		.then(users => {
 			res.status(200).send({ users });
+		})
+		.catch(err => next(err));
+};
+
+exports.getUserById = (req, res, next) => {
+	const { username } = req.params;
+
+	fetchUserById(username)
+		.then(user => {
+			res.status(200).send({ user });
 		})
 		.catch(err => next(err));
 };
