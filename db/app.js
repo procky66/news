867c5b29b3a12726b10266e2dd03fecac26a5ffd
procky66 <steveprocter69@googlemail.com>
2022-09-26 10:25:23 +0100

    allow cross origin resource sharing

--- db/app.js
+++ db/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 
 const router = require("./routes/router");
 const apiRouter = require("./routes/api.router");
 const { handle400s, handle404s, handle500s } = require("./errors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/", router);
